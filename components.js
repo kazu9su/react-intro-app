d0e2446eac3186126a4331603cf63c64ad123526
Tomy <kazushige.tominaga@mkt-apps.com>
2016-11-21 23:57:00 +0900

    add intro scripts

---
+++ components.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+
+export class BeerListContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      beers: []
+    };
+    this.addItem = this.addItem.bind(this)
+  }
+
+  render() {
+    return (
+        <div>
+          <InputArea onSubmit={this.addItem}/>
+          <BeerList items={this.state.beers}/>
+        </div>
+    )
+  }
+
+  addItem(name) {
+    this.setState({
+      beers: [].concat(this.state.beers).concat([name])
+    });
+  }
+}
+
+export class InputArea extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    };
+
+    this.setText = this.setText.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  setText(event) {
+    this.setState({text: event.target.value});
+  }
+
+  handleClick() {
+    this.props.onSubmit(this.state.text);
+  }
+
+  render() {
+    return (
+        <div>
+          <input value={this.state.text} onChange={this.setText}/>
+          <button onClick={this.handleClick}>Add</button>
+        </div>
+    )
+  }
+}
+
+export class BeerList extends Component {
+  render() {
+    return this.props.items ? (
+        <ul>
+        {this.props.items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+        </ul>
+    ) : null;
+  }
+}
+
+BeerList.propTypes = {
+  items: React.PropTypes.array.isRequired
+};
+
+InputArea.PropTypes = {
+  onSubmit: React.PropTypes.func.isRequired
+};
